0dce3d0a8b9d21803a5935f80f607892ec0d9ab4
juasda96 <juasda96@gmail.com>
2017-05-12 17:48:17 -0500

    Segunda parte del laboratorio

---
+++ customer-edgeserver/src/main/java/com/udea/CustomerEdgeserverApplication.java
@@ -0,0 +1,12 @@
+package com.udea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerEdgeserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerEdgeserverApplication.class, args);
+	}
+}
